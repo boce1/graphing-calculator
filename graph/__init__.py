7eef0256007dbff759204d5ace260d8882d6ad8d
Boyan Zarev <zarev.bojan2@gmail.com>
2024-07-29 19:12:11 +0300

    add colors, enabling and disabling dots, function read from file, change line width

--- graph/__init__.py
+++ graph/__init__.py
@@ -17,8 +17,21 @@ class Graph:
 
             except ZeroDivisionError:
                 self.y_values.append(None)
+        
+    def draw_dots(self, window, camera_x, camera_y, visible):
+        if visible:
+            for i in range(len(self.x_values)):
+                if self.y_values[i] != None:
+                    try:
+                        pygame.draw.circle(window, self.color, (camera_x + self.x_values[i], camera_y + self.y_values[i]), 3)
+                    except TypeError:
+                        pass # int overflow or complex number
 
-    def draw_dots(self, window, camera_x, camera_y):
-        for i in range(len(self.x_values)):
-            if self.y_values[i] != None:
-                pygame.draw.circle(window, self.color, (camera_x + self.x_values[i], camera_y + self.y_values[i]), 3)
+    def link_dots(self, window, camera_x, camera_y, visible):
+        for i in range(len(self.x_values) - 1):
+            if self.y_values[i] != None and self.y_values[i + 1] != None:
+                try:
+                    pygame.draw.line(window, self.color, (camera_x + self.x_values[i], camera_y + self.y_values[i]), (camera_x + self.x_values[i + 1], camera_y + self.y_values[i + 1]), 3)
+                except TypeError:
+                    pass # int overflow
+        self.draw_dots(window, camera_x, camera_y, visible)
--- main.py
+++ main.py
@@ -1,19 +1,26 @@
 import pygame
 from graph import Graph
-from math import floor
+#from interface import user_menu
+
 pygame.init()
 
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+CYAN = (0, 255, 255)
+PURPLE = (255, 0, 255)
+PINK = (255, 150, 150)
 
 window_width = 900
 window_height = 900
 grid_color = list(BLACK)
 background_color = list(WHITE)
-dot_color = list(RED)
+dot_colors = [list(RED), list(GREEN), list(BLUE), list(CYAN), list(PURPLE), list(PINK)]
 camera_x = 0
 camera_y = 0
+visible_dots = False
 
 dragging = False
 start_move_x = None
@@ -22,18 +29,29 @@ start_mouse_x = None
 start_mouse_y = None
 mouse_x, mouse_y = 0, 0
 
-limit_x = window_width
-limit_y = window_height
+limit_x = 4 * window_width
+limit_y = 4 * window_height
 
 unit = 10
 
 x_axes_values = [x for x in range(-limit_x - window_width // 2, window_width + limit_x + 1, unit)]
 y_axes_values = [x for x in range(-limit_y - window_height // 2, window_height + limit_y + 1, unit)]
 
+graphs = []
+
 pygame.display.set_caption("Graph calculator")
 window = pygame.display.set_mode((window_width, window_height))
 
-g1 = Graph(x_axes_values, window_width, window_height, unit, "x**0.5", dot_color)
+def create_graphs():
+    global graphs
+    index = 0
+    with open('graph_input.txt', 'r') as file:
+        for line in file:
+            if index >= len(dot_colors):
+                index = 0
+            stripped_line = line.strip()
+            graphs.append(Graph(x_axes_values, window_width, window_height, unit, stripped_line, dot_colors[index]))
+            index += 1
 
 def draw_dots():
     for i in range(len(x_axes_values)):
@@ -49,8 +67,9 @@ def draw_axis():
 def draw_scene():
     window.fill(background_color)
     draw_axis()
-    # draw_graph(x_values, y_values)
-    g1.draw_dots(window, camera_x, camera_y)
+    for graph in graphs:
+        graph.link_dots(window, camera_x, camera_y, visible_dots)
+
     pygame.display.update()
 
 def is_component_bright(component):
@@ -69,22 +88,34 @@ def invert_colors(event):
                 background_color[i] = 255 / 2 - abs(255 / 2 - background_color[i])
             else:
                 background_color[i] = 255 / 2 + abs(255 / 2 - background_color[i])
-            if is_component_bright(dot_color[i]):
-                dot_color[i] = 255 / 2 - abs(255 / 2 - dot_color[i])
-            else:
-                dot_color[i] = 255 / 2 + abs(255 / 2 - dot_color[i])
+            
+        for dot_color in dot_colors:
+            for i in range(3):
+                if is_component_bright(dot_color[i]):
+                    dot_color[i] = 255 / 2 - abs(255 / 2 - dot_color[i])
+                else:
+                    dot_color[i] = 255 / 2 + abs(255 / 2 - dot_color[i])
 
 def detect_moving(event):
     global start_move_x, start_move_y, dragging, start_mouse_x, start_mouse_y
     if event.type == pygame.MOUSEBUTTONDOWN and \
-        event.button == 2:
+        event.button == 1:
         start_move_x, start_move_y = camera_x, camera_y
         start_mouse_x, start_mouse_y = mouse_x, mouse_y
         dragging = True
     if event.type == pygame.MOUSEBUTTONUP and \
-        event.button == 2:
+        event.button == 1:
         start_move_x, start_move_y = None, None
         dragging = False
+
+def control_dots(event):
+    global visible_dots
+    if event.type == pygame.KEYDOWN \
+        and event.key == pygame.K_o:
+        if visible_dots:
+            visible_dots = False
+        else:
+            visible_dots = True
 
 def move_camera(mouse_x, mouse_y):
     global camera_x, camera_y
@@ -101,8 +132,9 @@ def move_camera(mouse_x, mouse_y):
         if camera_y < -limit_y or camera_y > limit_y:
             camera_y = temp_camera_y
 
-running = True
 
+running = True
+create_graphs()
 while running:
     mouse_x, mouse_y = pygame.mouse.get_pos()
 
@@ -111,6 +143,7 @@ while running:
             running = False
         invert_colors(event)
         detect_moving(event)
+        control_dots(event)
     draw_scene()
     move_camera(mouse_x, mouse_y)
 
